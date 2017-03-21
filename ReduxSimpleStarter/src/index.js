d5ee7eec6da57b676f94bf632ab930aa0eefe5ac
abc <ebookcode123@gmail.com>
2017-03-21 15:28:06 -0700

    add new for call linking

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component}from 'react';
 import ReactDOM from 'react-dom';
 
@@ -48,9 +49,15 @@ class App extends Component {
   }
 
   render () {
+
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)},300);
+
+
     return (
       <div>
-        <SearchBar onSearchTermChange = {term => this.videoSearch(term)}/>
+        {/*<SearchBar onSearchTermChange = {term => this.videoSearch(term)}/> */}
+        <SearchBar onSearchTermChange = {videoSearch}/>
+
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
         onVideoSelect={selectedVideo => this.setState({selectedVideo})}
